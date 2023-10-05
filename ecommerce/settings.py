a3ad67a4026b0bdf93e995923ece6d7e0a1640df
Karuppaiyan <ukaruppaiyan@gmail.com>
2023-10-05 23:52:43 +0530

    upload thalaimurai app add urlad1

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -134,7 +134,7 @@ STATIC_URL = 'static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
 STATICFILES_DIRS = [BASE_DIR, 'static']
-STATIC_ROOT = BASE_DIR, 'staticfiles','assets'
+STATIC_ROOT = BASE_DIR, 'staticfiles' + 'assets'
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR, "media"
