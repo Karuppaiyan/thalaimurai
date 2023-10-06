9cfea39da2b28a3c665633f2ca771bc3ca850604
Karuppaiyan <ukaruppaiyan@gmail.com>
2023-10-06 20:39:01 +0530

    upload thalaimurai update29

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -31,7 +31,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -87,12 +86,12 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 # DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 # }
 
@@ -100,12 +99,12 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 # DATABASE_URL = os.environ.get('DATABASE_URL')
 # DATABASES = {'default': dj_database_url.parse(DATABASE_URL)}
 
-DATABASES = {
-    'default': dj_database_url.config (        
-        # Feel free to alter this value to suit your needs.        
-        default='postgres://thalaimurai_db_user:OpzjCdT5lc0xNsAffhhfVSivqUC5sQ8T@dpg-ckfuj2ect0pc73dnls10-a.oregon-postgres.render.com/thalaimurai_db',   
-        conn_max_age=600 )
-        }
+# DATABASES = {
+#     'default': dj_database_url.config (        
+#         # Feel free to alter this value to suit your needs.        
+#         default='postgres://thalaimurai_db_user:OpzjCdT5lc0xNsAffhhfVSivqUC5sQ8T@dpg-ckfuj2ect0pc73dnls10-a.oregon-postgres.render.com/thalaimurai_db',   
+#         conn_max_age=600 )
+#         }
 
 
 # Password validation
