b4e04e93de3db9117bb0af913258043ffd6959fc
Karuppaiyan <ukaruppaiyan@gmail.com>
2023-10-06 19:12:58 +0530

    upload thalaimurai update26

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -27,9 +27,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='@0%m5m+aal2i_^87$adi_2kjnm4*=
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
-DEBUG = 'RENDER' not in os.environ
+DEBUG = True
 
-ALLOWED_HOSTS = ['www.thalaimurai.in']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
