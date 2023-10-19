fee9c63a24b08abcc5c7079f10e843cad4a9c9d8
Karuppaiyan <ukaruppaiyan@gmail.com>
2023-10-19 11:17:50 +0530

    first static project

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -149,7 +149,8 @@ STATIC_URL = 'static/'
 # ]
 # STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
-STATICFILES_DIRS = [BASE_DIR, 'static']
+STATICFILES_DIRS = [BASE_DIR, 'static','/var/www/static/']
+
 STATIC_ROOT = BASE_DIR, 'staticfiles' + 'assets'
 
 MEDIA_URL = "media/"
